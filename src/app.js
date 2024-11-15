d009250989b1099962ce36385c68624e68490252
Kathryn E Lloyd <71646243+kel-source@users.noreply.github.com>
2024-11-15 22:19:49 +0000

    Fixed js erroring out over trying to find components that don't exist on the page

--- src/app.js
+++ src/app.js
@@ -10,146 +10,154 @@
 // 
 
 // ROW-TYPE-AUDIO
-    document.addEventListener("DOMContentLoaded", (event) => {
-        const controlBtn = document.getElementById("play-pause");
-        let track = document.getElementById("track"),
-        duration = track.duration, // Duration of audio clip (NaN here as the track is not loaded)
-        playhead = document.getElementById("playhead"), // playhead
-        timeline = document.getElementById("timeline"), // timeline
-        timelineWidth = timeline.offsetWidth - playhead.offsetWidth,
-        source = controlBtn.getAttribute("data-audio"),
-        leftTimer = document.getElementById("is-left"),
-        rightTimer = document.getElementById("is-right"),
-        repeatButton = document.getElementsByClassName("icon-repeat");
-    
-        if (!track) return;
-    
-        // Update the track duration when the track is loaded.
-        track.addEventListener(
-        "canplaythrough",
-        function () {
-            duration = track.duration;
-        },
-        false
-        );
-    
-        function playPause() {
-        null === track.getAttribute("src") ? track.setAttribute("src", source) : "";
-    
-        if (track.paused) {
-            track.play();
-            controlBtn.className = "pause";
-        } else {
-            track.pause();
+    const controlBtn = document.getElementById("play-pause");
+
+    if (controlBtn !== null) {
+        document.addEventListener("DOMContentLoaded", (event) => {
+            let track = document.getElementById("track"),
+            duration = track.duration, // Duration of audio clip (NaN here as the track is not loaded)
+            playhead = document.getElementById("playhead"), // playhead
+            timeline = document.getElementById("timeline"), // timeline
+            timelineWidth = timeline.offsetWidth - playhead.offsetWidth,
+            source = controlBtn.getAttribute("data-audio"),
+            leftTimer = document.getElementById("is-left"),
+            rightTimer = document.getElementById("is-right"),
+            repeatButton = document.getElementsByClassName("icon-repeat");
+        
+            if (!track) return;
+        
+            // Update the track duration when the track is loaded.
+            track.addEventListener(
+            "canplaythrough",
+            function () {
+                duration = track.duration;
+            },
+            false
+            );
+        
+            function playPause() {
+            null === track.getAttribute("src") ? track.setAttribute("src", source) : "";
+        
+            if (track.paused) {
+                track.play();
+                controlBtn.className = "pause";
+            } else {
+                track.pause();
+                controlBtn.className = "play";
+            }
+            }
+        
+            controlBtn.addEventListener("click", playPause);
+        
+            track.addEventListener("ended", function () {
             controlBtn.className = "play";
-        }
-        }
-    
-        controlBtn.addEventListener("click", playPause);
-    
-        track.addEventListener("ended", function () {
-        controlBtn.className = "play";
-        track.currentTime = 0; // reset the playhead
-    
-        // Let's check if the repeat button is on, then play the track again.
-        let times = repeatButton[0].getAttribute("repeat");
-        if (times > 0) {
-            controlBtn.className = "";
-            controlBtn.className = "pause";
-            track.play();
-        }
+            track.currentTime = 0; // reset the playhead
+        
+            // Let's check if the repeat button is on, then play the track again.
+            let times = repeatButton[0].getAttribute("repeat");
+            if (times > 0) {
+                controlBtn.className = "";
+                controlBtn.className = "pause";
+                track.play();
+            }
+            });
+        
+            // timeupdate event listener
+            track.addEventListener("timeupdate", timeUpdate, false);
+        
+            // makes timeline clickable
+            timeline.addEventListener(
+            "click",
+            function (event) {
+                moveplayhead(event);
+                track.currentTime = duration * clickPercent(event);
+            },
+            false
+            );
+        
+            // Moves playhead as user clicks
+            function moveplayhead(event) {
+            let newMargLeft = event.clientX - getPosition(timeline);
+        
+            if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
+                playhead.style.marginLeft = newMargLeft + "px";
+            }
+            if (newMargLeft < 0) {
+                playhead.style.marginLeft = "0px";
+            }
+            if (newMargLeft > timelineWidth) {
+                playhead.style.marginLeft = timelineWidth + "px";
+            }
+            }
+        
+            function getPosition(el) {
+            return el.getBoundingClientRect().left;
+            }
+        
+            // returns decimal number of the total timelineWidth
+            function clickPercent(event) {
+            return (event.clientX - getPosition(timeline)) / timelineWidth;
+            }
+        
+            // Time update
+            function timeUpdate() {
+            let playPercent = timelineWidth * (track.currentTime / duration);
+            playhead.style.marginLeft = playPercent + "px";
+            scrub.style.width = playPercent + "px";
+        
+            // Current time
+            document.getElementById("duration-time").innerHTML = formatSecondsAsTime(
+                duration
+            );
+            document.getElementById("current-time").innerHTML = formatSecondsAsTime(
+                Math.floor(track.currentTime)
+            );
+            }
+        
+            // Synchronizes playhead position with current point in audio
+            function formatSecondsAsTime(secs, format) {
+            let hr = Math.floor(secs / 3600);
+            let min = Math.floor((secs - hr * 3600) / 60);
+            let sec = Math.floor(secs - hr * 3600 - min * 60);
+        
+            if (min < 10) {
+                min = "0" + min;
+            }
+            if (sec < 10) {
+                sec = "0" + sec;
+            }
+        
+            return min + ":" + sec;
+            }
         });
-    
-        // timeupdate event listener
-        track.addEventListener("timeupdate", timeUpdate, false);
-    
-        // makes timeline clickable
-        timeline.addEventListener(
-        "click",
-        function (event) {
-            moveplayhead(event);
-            track.currentTime = duration * clickPercent(event);
-        },
-        false
-        );
-    
-        // Moves playhead as user clicks
-        function moveplayhead(event) {
-        let newMargLeft = event.clientX - getPosition(timeline);
-    
-        if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
-            playhead.style.marginLeft = newMargLeft + "px";
-        }
-        if (newMargLeft < 0) {
-            playhead.style.marginLeft = "0px";
-        }
-        if (newMargLeft > timelineWidth) {
-            playhead.style.marginLeft = timelineWidth + "px";
-        }
-        }
-    
-        function getPosition(el) {
-        return el.getBoundingClientRect().left;
-        }
-    
-        // returns decimal number of the total timelineWidth
-        function clickPercent(event) {
-        return (event.clientX - getPosition(timeline)) / timelineWidth;
-        }
-    
-        // Time update
-        function timeUpdate() {
-        let playPercent = timelineWidth * (track.currentTime / duration);
-        playhead.style.marginLeft = playPercent + "px";
-        scrub.style.width = playPercent + "px";
-    
-        // Current time
-        document.getElementById("duration-time").innerHTML = formatSecondsAsTime(
-            duration
-        );
-        document.getElementById("current-time").innerHTML = formatSecondsAsTime(
-            Math.floor(track.currentTime)
-        );
-        }
-    
-        // Synchronizes playhead position with current point in audio
-        function formatSecondsAsTime(secs, format) {
-        let hr = Math.floor(secs / 3600);
-        let min = Math.floor((secs - hr * 3600) / 60);
-        let sec = Math.floor(secs - hr * 3600 - min * 60);
-    
-        if (min < 10) {
-            min = "0" + min;
-        }
-        if (sec < 10) {
-            sec = "0" + sec;
-        }
-    
-        return min + ":" + sec;
-        }
-    });
+    }
 //
 
 // HIDDEN TEXT REVEAL BY BUTTON CLICK (see business anchor section on dev page)
-const hiddenText = document.querySelector('#hiddenText');
-const hiddenButton = document.querySelector('#hiddenButton');
+    const hiddenText = document.querySelector('#hiddenText');
 
-hiddenButton.addEventListener('click', () => {
-    hiddenText.classList.toggle('hidden');
+    if (hiddenText !== null) {
+        const hiddenButton = document.querySelector('#hiddenButton');
 
-    if (hiddenText.classList.contains('hidden')) {
-        hiddenButton.innerHTML = 'Show me the template!';
-    } else {
-        hiddenButton.innerHTML = 'Hide the template!';
+        hiddenButton.addEventListener('click', () => {
+            hiddenText.classList.toggle('hidden');
+
+            if (hiddenText.classList.contains('hidden')) {
+                hiddenButton.innerHTML = 'Show me the template!';
+            } else {
+                hiddenButton.innerHTML = 'Hide the template!';
+            }
+        });
     }
-});
 // 
   
 // ROW-TYPE-TICKER
-// nothing below this because it's erroring out
-var ticker = document.querySelector('.ticker');
-    list = document.querySelector('.ticker-list');
-    clone = list.cloneNode(true);
+    var ticker = document.querySelector('.ticker');
 
-ticker.append(clone)
+    if (ticker !== null) {
+        var list = document.querySelector('.ticker-list');
+        clone = list.cloneNode(true);
+
+        ticker.append(clone)
+    }
 // 
